6e0e188142e0d3699123982960c40a5efdb2f596
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-27 23:53:40 +0600

    feat(featured): Implements featured for home page

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import SectionTitle from '../../../components/SectionTitle/SectionTitle'
+import React from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import featuredImg from "../../../assets/home/featured.jpg";
+import "./Featured.css";
 
 const Featured = () => {
   return (
-    <section>
-        <SectionTitle subHeading={"Check it out"} heading={"Featured Item"}></SectionTitle>
+    <section className="featured-item text-white pt-8 my-20">
+      <SectionTitle
+        subHeading={"Check it out"}
+        heading={"Featured Item"}
+      ></SectionTitle>
+      <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+        <div>
+          <img src={featuredImg} alt="" />
+        </div>
+        <div className="md:ml-10">
+          <p>Jun 27, 2024</p>
+          <p className="uppercase">Where can we get some?</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit perferendis optio iure autem dicta hic delectus, esse
+            similique dignissimos alias consequatur, sint veritatis suscipit
+            quibusdam tenetur dolores nesciunt, animi ipsum. Consectetur, a
+            totam! Labore autem exercitationem maxime aliquam sapiente magnam.
+          </p>
+          <button className="btn btn-outline">Order Now</button>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
