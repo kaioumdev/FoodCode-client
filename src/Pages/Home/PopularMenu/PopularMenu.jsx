45a36205cbb97fad05acf3f3765814ec11ebc03c
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-27 22:01:02 +0600

    feat(menuItem): Implements menuItem for popularMenu

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import SectionTitle from '../../../components/SectionTitle/SectionTitle'
+import React, { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
+  const [menu, setMenu] = useState([]);
+  console.log(menu);
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const popularItems = data.filter((item) => item.category === "popular");
+        setMenu(popularItems);
+      });
+  }, []);
   return (
     <section>
-        <SectionTitle heading={"From Our Menu"} subHeading={"Check it out"}></SectionTitle>
+      <SectionTitle
+        heading={"From Our Menu"}
+        subHeading={"Check it out"}
+      ></SectionTitle>
+      <div className="grid md:grid-cols-2 gap-4">
+        {menu.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default PopularMenu
+export default PopularMenu;
