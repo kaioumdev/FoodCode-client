5c2270d3bbedf583df3f527bba6309bd5219cd64
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-27 21:39:02 +0600

    feat(popularMenu): create popular menu & implements title dynamically to this section

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle'
+
+const PopularMenu = () => {
+  return (
+    <section>
+        <SectionTitle heading={"From Our Menu"} subHeading={"Check it out"}></SectionTitle>
+    </section>
+  )
+}
+
+export default PopularMenu
--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SectionTitle = ({heading, subHeading}) => {
   return (
     <div className='md:w-4/12 mx-auto text-center my-8'>
-        <p className='text-yellow-600 mb-2'>{subHeading}</p>
+        <p className='text-yellow-600 mb-2'>-------- {subHeading} --------</p>
         <h3 className='border-y-4 py-4 text-3xl uppercase'>{heading}</h3>
     </div>
   )
