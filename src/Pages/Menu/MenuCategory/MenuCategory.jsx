ebfc9e2e60a5986b911568051315f8e883b07d7e
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-28 23:09:22 +0600

    feat(menu): updated & styles menuCategory

--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -4,9 +4,9 @@ import Cover from "../../Shared/Cover/Cover";
 
 const MenuCategory = ({ items, title, img }) => {
   return (
-    <div>
+    <div className="p-8">
       {title && <Cover img={img} title={title}></Cover>}
-      <div className="grid md:grid-cols-2 gap-4">
+      <div className="grid md:grid-cols-2 gap-4 my-20">
         {items.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
