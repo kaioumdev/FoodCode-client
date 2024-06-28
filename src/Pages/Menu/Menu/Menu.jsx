c1f0697fd7975bf737ef8fdf8f828454d16a46d6
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-28 18:36:23 +0600

    feat(cover): create cover page & implements react-parallax for cover

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import Cover from '../../Shared/Cover/Cover'
+import menuImg from "../../../assets/menu/banner3.jpg"
+import PopularMenu from '../../Home/PopularMenu/PopularMenu'
 
 const Menu = () => {
   return (
@@ -7,6 +10,9 @@ const Menu = () => {
         <Helmet>
             <title>Bistro Boss | Menu</title>
         </Helmet>
+        <Cover img={menuImg} title="our menu"></Cover>
+        <PopularMenu></PopularMenu>
+        <h3>Menu</h3>
     </div>
   )
 }
