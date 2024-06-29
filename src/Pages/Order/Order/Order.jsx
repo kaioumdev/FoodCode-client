c65845122c89594ef87a47b507ca9cc878ae89a4
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-29 18:50:15 +0600

    feat(server): get all menu data from server

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -6,19 +6,25 @@ import "react-tabs/style/react-tabs.css";
 import useMenu from "../../../hooks/useMenu";
 import OrderTab from "../OrderTab/OrderTab";
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Order = () => {
-    const [tabIndex, setTabIndex] = useState(0);
-    const [menu] = useMenu();
-    const desserts = menu.filter((item) => item.category === 'dessert');
-    const salads = menu.filter((item) => item.category === 'salad');
-    const pizzas = menu.filter((item) => item.category === 'pizza');
-    const drinks = menu.filter((item) => item.category === 'drinks');
-    const soups = menu.filter((item) => item.category === 'soup');
-    const {category} = useParams()
-    console.log(category)
+  const categories = ["salads", "pizzas", "soups", "desserts", "drinks"];
+  const { category } = useParams();
+  const initialIndex = categories.indexOf(category);
+  const [tabIndex, setTabIndex] = useState(initialIndex);
+  const [menu] = useMenu();
+  console.log(tabIndex)
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const salads = menu.filter((item) => item.category === "salad");
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const drinks = menu.filter((item) => item.category === "drinks");
+  const soups = menu.filter((item) => item.category === "soup");
   return (
     <div>
+             <Helmet>
+            <title>Bistro Boss | Order</title>
+        </Helmet>
       <Cover img={shopImg} title="Order Shop"></Cover>
       <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
@@ -29,19 +35,19 @@ const Order = () => {
           <Tab>Drinks</Tab>
         </TabList>
         <TabPanel>
-            <OrderTab items={salads}></OrderTab>
+          <OrderTab items={salads}></OrderTab>
         </TabPanel>
         <TabPanel>
-            <OrderTab items={pizzas}></OrderTab>
+          <OrderTab items={pizzas}></OrderTab>
         </TabPanel>
         <TabPanel>
-            <OrderTab items={soups}></OrderTab>
+          <OrderTab items={soups}></OrderTab>
         </TabPanel>
         <TabPanel>
-            <OrderTab items={desserts}></OrderTab>
+          <OrderTab items={desserts}></OrderTab>
         </TabPanel>
         <TabPanel>
-            <OrderTab items={drinks}></OrderTab>
+          <OrderTab items={drinks}></OrderTab>
         </TabPanel>
       </Tabs>
     </div>
--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -6,7 +6,7 @@ const useMenu = () => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch('menu.json')
+        fetch('http://localhost:5000/menu')
        .then(res => res.json())
        .then(data => {
            setMenu(data)
