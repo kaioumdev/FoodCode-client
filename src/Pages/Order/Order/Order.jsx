f2b7138e4844f301c3ed4a2b5c8f7f2ffb820314
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-29 12:28:17 +0600

    feat(salads): show salads to tabpanel

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -3,9 +3,18 @@ import shopImg from "../../../assets/shop/banner2.jpg";
 import Cover from "../../Shared/Cover/Cover";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import FoodCard from "../../../components/FoodCard/FoodCard";
+import useMenu from "../../../hooks/useMenu";
+import OrderTab from "../OrderTab/OrderTab";
 
 const Order = () => {
     const [tabIndex, setTabIndex] = useState(0);
+    const [menu] = useMenu();
+    const desserts = menu.filter((item) => item.category === 'dessert');
+    const salads = menu.filter((item) => item.category === 'salad');
+    const pizzas = menu.filter((item) => item.category === 'pizza');
+    const offered = menu.filter((item) => item.category === 'offered');
+    const soups = menu.filter((item) => item.category === 'soup');
   return (
     <div>
       <Cover img={shopImg} title="Order Shop"></Cover>
@@ -17,10 +26,18 @@ const Order = () => {
           <Tab>Dessert</Tab>
           <Tab>Drinks</Tab>
         </TabList>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
+        <TabPanel>
+            <OrderTab items={salads}></OrderTab>
+        </TabPanel>
+        {/* <TabPanel>
+            <OrderTab items={pizzas}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+            <OrderTab items={soups}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+            <OrderTab items={desserts}></OrderTab>
+        </TabPanel> */}
         <TabPanel></TabPanel>
       </Tabs>
     </div>
