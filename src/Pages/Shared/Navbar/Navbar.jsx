8f8bd8585774dd6b7e6755af59226e7fd62c2ec7
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-30 09:42:14 +0600

    feat(login): create login page & use location to hide navbar, footer from login page

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -14,6 +14,9 @@ const Navbar = () => {
       <li>
         <Link to="/order/salads">Order</Link>
       </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
     </>
   );
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <div className="max-w-screen-xl mx-auto">
-  <HelmetProvider>
-  <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
-  </HelmetProvider>
-   </div>
+    <div className="max-w-screen-xl mx-auto">
+      <HelmetProvider>
+        <RouterProvider router={router}>
+          <App />
+        </RouterProvider>
+      </HelmetProvider>
+    </div>
   </React.StrictMode>
 );
