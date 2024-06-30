2006b439ea6bc08483a13df537ae35ec91c0fc77
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-30 11:46:36 +0600

    feat(context): Implements createContext for authProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="max-w-screen-xl mx-auto">
+      <AuthProvider>
       <HelmetProvider>
         <RouterProvider router={router}>
           <App />
         </RouterProvider>
       </HelmetProvider>
+      </AuthProvider>
     </div>
   </React.StrictMode>
 );
