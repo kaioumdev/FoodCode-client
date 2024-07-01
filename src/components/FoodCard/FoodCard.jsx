193282dea2b1512af7ee217e425cea5de593fc59
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-01 20:20:27 +0600

    feat(useAuth): create custom auth for users

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FoodCard = ({item}) => {
     const {name, image, price, recipe} = item;
-    const handleButtonClick = (food) => {
+    const handleAddToCart = (food) => {
       console.log(food)
     }
   return (
@@ -18,7 +18,7 @@ const FoodCard = ({item}) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button onClick={() =>handleButtonClick(item)} className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Add to Cart</button>
+          <button onClick={() =>handleAddToCart(item)} className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Add to Cart</button>
         </div>
       </div>
     </div>
