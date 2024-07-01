c3cbc154cd0a165d2ca4ca502af139bf67f22eed
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-01 20:03:40 +0600

    feat(foodItem): get food item when click add to cart button

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 const FoodCard = ({item}) => {
     const {name, image, price, recipe} = item;
+    const handleButtonClick = (food) => {
+      console.log(food)
+    }
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure>
@@ -15,7 +18,7 @@ const FoodCard = ({item}) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Add to Cart</button>
+          <button onClick={() =>handleButtonClick(item)} className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Add to Cart</button>
         </div>
       </div>
     </div>
