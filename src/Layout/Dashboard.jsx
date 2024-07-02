43ddf5124469a0597f1c0a8ebde033dcf9a01991
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-03 03:26:19 +0600

    feat(dashboard): add navbar page at dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
 import { MdReviews } from "react-icons/md";
+import { TiThMenuOutline } from "react-icons/ti";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-orange-400">
-        <ul className="menu">
+        <ul className="menu p-4">
         <li>
             <NavLink to="dashboard/userHome">
               <FaHome></FaHome>
@@ -36,6 +37,25 @@ const Dashboard = () => {
             <NavLink to="dashboard/bookings">
               <FaList></FaList>
               My Bookings
+            </NavLink>
+          </li>
+        <div className="divider"></div>
+        <li>
+            <NavLink to="/">
+              <FaHome></FaHome>
+              Home 
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/menu">
+              <TiThMenuOutline></TiThMenuOutline>
+              Menu 
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salads">
+              <FaList></FaList>
+              Order 
             </NavLink>
           </li>
         </ul>
