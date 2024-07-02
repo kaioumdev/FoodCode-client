cdc5f5d602bad60d3df0eae13995c661bc2b0daf
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-03 02:16:33 +0600

    feat(dashboard): create dashboard page & show dashboard list

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
-import { NavLink, Outlet } from 'react-router-dom'
+import React from "react";
+import { FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import { MdReviews } from "react-icons/md";
+import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div className='flex'>
-        <div className='w-64 min-h-screen bg-orange-400'>
-            <ul className='menu'>
-                <li><NavLink to="dashboard/cart">My Cart</NavLink></li>
-            </ul>
-        </div>
-        <div className="flex-1">
-            <Outlet></Outlet>
-        </div>
+    <div className="flex">
+      <div className="w-64 min-h-screen bg-orange-400">
+        <ul className="menu">
+        <li>
+            <NavLink to="dashboard/userHome">
+              <FaHome></FaHome>
+              User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="dashboard/cart">
+              <FaShoppingCart></FaShoppingCart>
+              My Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="dashboard/reservation">
+              <FaCalendar></FaCalendar>
+              Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="dashboard/review">
+              <MdReviews></MdReviews>
+              Add a Review
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="dashboard/bookings">
+              <FaList></FaList>
+              My Bookings
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="flex-1">
+        <Outlet></Outlet>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
