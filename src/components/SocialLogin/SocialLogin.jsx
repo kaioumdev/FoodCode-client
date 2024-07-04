ac6698721f98f58dd4161497a3b4851ece76f616
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-04 12:22:26 +0600

    feat(user): post user info to server

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import useAuth from "../../hooks/useAuth";
 import { FaGoogle } from "react-icons/fa";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
   const { signInWithGoogle, updateUserProfile } = useAuth();
+  const axiosPublic = useAxiosPublic();
+  const navigate = useNavigate();
   const handleGoogleSignIn = () => {
     signInWithGoogle()
       .then((result) => {
         console.log(result.user);
-        updateUserProfile(result.user.displayName, result.user.photoURL);
+        // updateUserProfile(result.user.displayName, result.user.photoURL);
+        const userInfo = {
+          name: result.user.displayName,
+          email: result.user.email,
+          photoURL: result.user.photoURL,
+        }
+        axiosPublic.post("/users", userInfo)
+        .then(res => {
+          console.log(res.data);
+          navigate("/");
+        })
       })
       .catch((error) => {
         console.log(error);
