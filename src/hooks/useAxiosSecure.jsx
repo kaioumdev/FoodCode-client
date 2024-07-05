18d49edcb0347812f9c61542fe921966293c6a02
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-06 05:50:14 +0600

    feat(interceptors): request interceptors to authorized headers for every secure call to the api

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,6 +5,17 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+  //request interceptor to authorize headers for every secure call to the api
+axiosSecure.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('access-token')
+  console.log('request stopped by interceptor')
+  config.headers.Authorization = `Bearer ${token}`
+  // Do something before request is sent
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
   return axiosSecure;
 }
 
