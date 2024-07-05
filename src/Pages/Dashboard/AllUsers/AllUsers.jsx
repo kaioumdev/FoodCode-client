c1eb0ec985d7a43e40c2831ba5816eee32c8ded8
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-05 19:46:12 +0600

    eat(users): show all users & create user delete button to delete user

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
@@ -11,6 +13,32 @@ const AllUsers = () => {
       return res.data;
     },
   });
+
+  const handleDelete = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      console.log(result);
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/user/${user._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch()
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  }
   return (
     <div>
       <div className="flex justify-evenly my-4">
@@ -25,17 +53,33 @@ const AllUsers = () => {
               <tr>
                 <th></th>
                 <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th>Email</th>
+                <th>Roll</th>
+                <th>Action</th>
               </tr>
             </thead>
             <tbody>
-              {users.map((user) => (
+              {users.map((user, index) => (
                 <tr key={user._id}>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
+                  <th>{index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                  <button
+                  onClick={() => handleMakeAdmin(user)}
+                  className="btn btn-lg bg-orange-500 xl"
+                >
+                  <FaUsers></FaUsers>
+                </button>
+                  </td>
+                  <td>
+                  <button
+                  onClick={() => handleDelete(user)}
+                  className="btn btn-ghost btn-lg text-red-600 xl"
+                >
+                  <FaTrashAlt></FaTrashAlt>
+                </button>
+                    </td>
                 </tr>
               ))}
             </tbody>
