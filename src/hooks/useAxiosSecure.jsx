2b7dccdf0dd98691b5c6efa94d3a049081077744
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-06 06:02:37 +0600

    feat(status): implements inerceptor to get status code

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,22 +1,41 @@
-import axios from 'axios'
+import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
-})
+  baseURL: "http://localhost:5000",
+});
 
 const useAxiosSecure = () => {
   //request interceptor to authorize headers for every secure call to the api
-axiosSecure.interceptors.request.use(function (config) {
-  const token = localStorage.getItem('access-token')
-  console.log('request stopped by interceptor')
-  config.headers.Authorization = `Bearer ${token}`
-  // Do something before request is sent
-  return config;
-}, function (error) {
-  // Do something with request error
-  return Promise.reject(error);
-});
-  return axiosSecure;
-}
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("request stopped by interceptor");
+      config.headers.Authorization = `Bearer ${token}`;
+      // Do something before request is sent
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
 
-export default useAxiosSecure
+  // Add a response interceptor
+  axios.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    function (error) {
+      const status = error.response.status;
+      console.log("status error in the interceptor", status);
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      return Promise.reject(error);
+    }
+  );
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
