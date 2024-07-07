4af7e25470b5f78217735d954265bdd4459d33ae
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-07 12:06:50 +0600

    feat(updated): updated AllUsers & useAxiosSecure pages

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,3 +1,123 @@
+// import React from "react";
+// import useAxiosSecure from "../../../hooks/useAxiosSecure";
+// import { useQuery } from "@tanstack/react-query";
+// import { FaTrashAlt, FaUsers } from "react-icons/fa";
+// import Swal from "sweetalert2";
+
+// const AllUsers = () => {
+//   const axiosSecure = useAxiosSecure();
+//   const { data: users = [], refetch } = useQuery({
+//     queryKey: ["users"],
+//     queryFn: async () => {
+//       const res = await axiosSecure.get("/users");
+//       // {
+//       //   headers: {
+//       //     authorization: `Bearer ${localStorage.getItem("access-token")}`,
+//       //   },
+//       // }
+//       return res.data;
+//     },
+//   });
+
+//   const handleDelete = (user) => {
+//     Swal.fire({
+//       title: "Are you sure?",
+//       text: "You won't be able to revert this!",
+//       icon: "warning",
+//       showCancelButton: true,
+//       confirmButtonColor: "#3085d6",
+//       cancelButtonColor: "#d33",
+//       confirmButtonText: "Yes, delete it!",
+//     }).then((result) => {
+//       console.log(result);
+//       if (result.isConfirmed) {
+//         axiosSecure.delete(`/user/${user._id}`).then((res) => {
+//           if (res.data.deletedCount > 0) {
+//             refetch();
+//             Swal.fire({
+//               title: "Deleted!",
+//               text: "Your file has been deleted.",
+//               icon: "success",
+//             });
+//           }
+//         });
+//       }
+//     });
+//   };
+
+//   const handleMakeAdmin = (user) => {
+//     axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+//       console.log(res.data);
+//       if (res.data.modifiedCount > 0) {
+//         refetch();
+//         Swal.fire({
+//           position: "top-end",
+//           icon: "success",
+//           title: `${user?.name} in an Admin now!`,
+//           showConfirmButton: false,
+//           timer: 1500,
+//         });
+//       }
+//     });
+//   };
+//   return (
+//     <div>
+//       <div className="flex justify-evenly my-4">
+//         <h1>All Users</h1>
+//         <h1>Total Users: {users.length}</h1>
+//       </div>
+//       <div>
+//         <div className="overflow-x-auto">
+//           <table className="table table-zebra">
+//             {/* head */}
+//             <thead>
+//               <tr>
+//                 <th></th>
+//                 <th>Name</th>
+//                 <th>Email</th>
+//                 <th>Role</th>
+//                 <th>Action</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {users.map((user, index) => (
+//                 <tr key={user._id}>
+//                   <th>{index + 1}</th>
+//                   <td>{user.name}</td>
+//                   <td>{user.email}</td>
+//                   <td>
+//                     {user.role === 'admin' ? (
+//                       "Admin"
+//                     ) : (
+//                       <button
+//                         onClick={() => handleMakeAdmin(user)}
+//                         className="btn btn-lg bg-orange-500 xl"
+//                       >
+//                         <FaUsers></FaUsers>
+//                       </button>
+//                     )}
+//                   </td>
+//                   <td>
+//                     <button
+//                       onClick={() => handleDelete(user)}
+//                       className="btn btn-ghost btn-lg text-red-600 xl"
+//                     >
+//                       <FaTrashAlt></FaTrashAlt>
+//                     </button>
+//                   </td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </table>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AllUsers;
+
+
 import React from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
@@ -6,15 +126,11 @@ import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
+  
   const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
-      // {
-      //   headers: {
-      //     authorization: `Bearer ${localStorage.getItem("access-token")}`,
-      //   },
-      // }
       return res.data;
     },
   });
@@ -29,14 +145,13 @@ const AllUsers = () => {
       cancelButtonColor: "#d33",
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
-      console.log(result);
       if (result.isConfirmed) {
-        axiosSecure.delete(`/user/${user._id}`).then((res) => {
+        axiosSecure.delete(`/users/${user._id}`).then((res) => {
           if (res.data.deletedCount > 0) {
             refetch();
             Swal.fire({
               title: "Deleted!",
-              text: "Your file has been deleted.",
+              text: "User has been deleted.",
               icon: "success",
             });
           }
@@ -47,19 +162,19 @@ const AllUsers = () => {
 
   const handleMakeAdmin = (user) => {
     axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
-      console.log(res.data);
       if (res.data.modifiedCount > 0) {
         refetch();
         Swal.fire({
           position: "top-end",
           icon: "success",
-          title: `${user?.name} in an Admin now!`,
+          title: `${user?.name} is now an Admin!`,
           showConfirmButton: false,
           timer: 1500,
         });
       }
     });
   };
+
   return (
     <div>
       <div className="flex justify-evenly my-4">
@@ -69,7 +184,6 @@ const AllUsers = () => {
       <div>
         <div className="overflow-x-auto">
           <table className="table table-zebra">
-            {/* head */}
             <thead>
               <tr>
                 <th></th>
@@ -93,7 +207,7 @@ const AllUsers = () => {
                         onClick={() => handleMakeAdmin(user)}
                         className="btn btn-lg bg-orange-500 xl"
                       >
-                        <FaUsers></FaUsers>
+                        <FaUsers />
                       </button>
                     )}
                   </td>
@@ -102,7 +216,7 @@ const AllUsers = () => {
                       onClick={() => handleDelete(user)}
                       className="btn btn-ghost btn-lg text-red-600 xl"
                     >
-                      <FaTrashAlt></FaTrashAlt>
+                      <FaTrashAlt />
                     </button>
                   </td>
                 </tr>
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,17 +1,67 @@
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import useAuth from "./useAuth";
+
+// const axiosSecure = axios.create({
+//   baseURL: "http://localhost:5000",
+// });
+
+// const useAxiosSecure = () => {
+//   const navigate = useNavigate();
+//   const {logOut} = useAuth()
+//   axiosSecure.interceptors.request.use(
+//     function (config) {
+//       const token = localStorage.getItem("access-token");
+//       console.log("request stopped by interceptor", token);
+//       config.headers.authorization = `Bearer ${token}`;
+//       return config;
+//     },
+//     function (error) {
+//       return Promise.reject(error);
+//     }
+//   );
+
+//   //intercepts 401 & 403 status
+//   axios.interceptors.response.use(
+//     function (response) {
+//       return response;
+//     },
+//     async(error) => {
+//       const status = error.response.status;
+//       console.log("Status error in the interceptors response", status);
+//       if (status === 401 || status === 403) {
+//         await logOut();
+//         navigate("/login");
+//       }
+//       return Promise.reject(error);
+//     }
+//   );
+//   return axiosSecure;
+// };
+
+// export default useAxiosSecure;
+
+// useAxiosSecure.jsx
+
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:5000", // Ensure this matches your backend port
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+
   axiosSecure.interceptors.request.use(
     function (config) {
       const token = localStorage.getItem("access-token");
-      console.log("request stopped by interceptor", token);
-      config.headers.authorization = `Bearer ${token}`;
+      console.log("Request stopped by interceptor", token);
+      if (token) {
+        config.headers.authorization = `Bearer ${token}`;
+      }
       return config;
     },
     function (error) {
@@ -19,17 +69,21 @@ const useAxiosSecure = () => {
     }
   );
 
-  //intercepts 401 & 403 status
-  axios.interceptors.response.use(
+  axiosSecure.interceptors.response.use(
     function (response) {
       return response;
     },
-    (error) => {
-      const status = error.response.status;
+    async (error) => {
+      const status = error.response ? error.response.status : null;
       console.log("Status error in the interceptors response", status);
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
       return Promise.reject(error);
     }
   );
+
   return axiosSecure;
 };
 
