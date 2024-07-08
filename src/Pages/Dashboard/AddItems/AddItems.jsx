a9a5f3bd69c88ba3b8c2b83096c39ca03ac5ad79
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-08 12:40:16 +0600

    feat(form): create addItems form by using react-hook-form

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,11 +1,34 @@
-import SectionTitle from "../../../components/SectionTitle/SectionTitle"
+import { useForm } from "react-hook-form";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const AddItems = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
-        <SectionTitle heading="add an Item" subHeading="What's new?"></SectionTitle>
+      <SectionTitle
+        heading="add an Item"
+        subHeading="What's new?"
+      ></SectionTitle>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("name")} />
+        <select
+          {...register("category")}
+          className="select select-bordered w-full max-w-xs"
+        >
+          <option disabled selected>
+            Select a Category?
+          </option>
+          <option value="salad">Salad</option>
+          <option value="pizza">Pizza</option>
+          <option value="soup">Soup</option>
+          <option value="dessert">Dessert</option>
+          <option value="drinks">Drinks</option>
+        </select>
+        <input type="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddItems
+export default AddItems;
