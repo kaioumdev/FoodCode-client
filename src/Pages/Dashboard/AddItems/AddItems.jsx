d282587a05ecf4d0e1076a2e5cd46e782db401a0
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-09 03:21:25 +0600

    feat(create): create new menuItem by submitting form to applying post method to create new menuItem

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
@@ -9,13 +10,26 @@ const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_ke
 const AddItems = () => {
   const { register, handleSubmit } = useForm();
   const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
   const onSubmit = async(data) => {
     console.log(data)
     const imageFile = {image: data.image[0]}
     const res = await axiosPublic.post(image_hosting_api, imageFile, {
       headers: { 'Content-Type': 'multipart/form-data' },
     })
-    console.log(res.data)
+    if(res.data.success){
+      //now send the menu item data to the server with the image url
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: data.price,
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      }
+      const menuRes = await axiosSecure.post("/menu", menuItem)
+      console.log(menuRes.data)
+    }
+    console.log('with img url', res.data)
   };
   return (
     <div>
