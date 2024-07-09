2391c2b6c27f1069cca9d572e2bd83a58ff7cc37
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-09 12:44:00 +0600

    feat(delete): delete button work successfully to remove item

--- src/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,10 +1,37 @@
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const ManageItems = () => {
   const [menu] = useMenu();
-  const handleDeleteItem = (item) => {};
+  const axiosSecure = useAxiosSecure();
+  const handleDeleteItem = (item) => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+            axiosSecure.delete(`/menu/${item._id}`)
+            .then(res => {
+                console.log(res.data);
+                if(res.data.deletedCount > 0) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                      });
+                }
+            })
+        }
+      });
+  };
   return (
     <div>
       <SectionTitle
