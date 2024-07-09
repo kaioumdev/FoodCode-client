f0d31557292d5af28385f24f58623e9cdccf6a59
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-09 10:32:08 +0600

    feat(title): add sectionTitle to manageItems page

---
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -0,0 +1,9 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle"
+
+const ManageItems = () => {
+  return (
+   <SectionTitle heading="Manage All Items" subHeading="Hurry Up"></SectionTitle>
+  )
+}
+
+export default ManageItems
