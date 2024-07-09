8515f4a9006186b9b39cb02d45762c50e44fbf36
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-10 01:58:54 +0600

    feat(title): add sectionTitle to the payment page

---
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,9 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Payment = () => {
+  return <div>
+    <SectionTitle heading="Payment" subHeading="Please pay to eat"></SectionTitle>
+  </div>;
+};
+
+export default Payment;
