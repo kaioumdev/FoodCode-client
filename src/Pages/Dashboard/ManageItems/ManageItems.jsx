754fe4fedadd347db55ea8a345412ec19566b055
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-09 12:06:41 +0600

    feat(manageItem): create manageItems ui and show all manage Items

--- src/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,9 +1,65 @@
-import SectionTitle from "../../../components/SectionTitle/SectionTitle"
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
 
 const ManageItems = () => {
+  const [menu] = useMenu();
+  const handleDeleteItem = (item) => {};
   return (
-   <SectionTitle heading="Manage All Items" subHeading="Hurry Up"></SectionTitle>
-  )
-}
+    <div>
+      <SectionTitle
+        heading="Manage All Items"
+        subHeading="Hurry Up"
+      ></SectionTitle>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {menu.map((item, index) => (
+                <tr key={item._id}>
+                  <th>{index + 1}</th>
+                  <td>
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      width="50"
+                      height="50"
+                    />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>
+                    <button className="btn btn-ghost btn-md bg-orange-500">
+                      <FaEdit></FaEdit>
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleDeleteItem(item)}
+                      className="btn btn-ghost btn-md bg-orange-500"
+                    >
+                      <FaTrashAlt></FaTrashAlt>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ManageItems
+export default ManageItems;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -16,6 +16,7 @@ import Cart from "../Pages/Dashboard/Cart/Cart";
 import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 import AddItems from "../Pages/Dashboard/AddItems/AddItems";
 import AdminRoute from "./AdminRoute";
+import ManageItems from "../Pages/Dashboard/ManageItems/ManageItems";
 
 export const router = createBrowserRouter([
     {
@@ -62,6 +63,10 @@ export const router = createBrowserRouter([
           path: "addItems",
           element:<AdminRoute><AddItems></AddItems></AdminRoute>
           // element: <AddItems />,
+        },
+        {
+          path: "manageItems",
+          element: <AdminRoute><ManageItems></ManageItems></AdminRoute>
         },
         {
           path: "users",
