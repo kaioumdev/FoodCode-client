3a69e88f4f16b2ba65cda8181f8e4f2d64cf48f5
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-09 20:40:39 +0600

    feat(updated): created updated method to updated menu item successfully

--- src/Pages/Dashboard/UpdateItem/UpdateItem.jsx
+++ src/Pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -1,48 +1,124 @@
 import { useLoaderData } from "react-router-dom";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-import { useState } from "react";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import { useForm } from "react-hook-form";
+import { FaUtensils } from "react-icons/fa";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 
 const UpdateItem = () => {
-  const item = useLoaderData();
-  const [name, setName] = useState(item.name);
-  const [price, setPrice] = useState(item.price);
-
-  const handleUpdate = async () => {
-    const updatedItem = { name, price };
-    const response = await fetch(`http://localhost:5000/menu/${item._id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(updatedItem)
-    });
-    if (response.ok) {
-      console.log("Item updated successfully");
-    } else {
-      console.error("Failed to update item");
+  const {name, category, price, recipe, image, _id} = useLoaderData();
+  // console.log(item);
+  const { register, handleSubmit, reset } = useForm();
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+  const onSubmit = async(data) => {
+    console.log(data)
+    const imageFile = {image: data.image[0]}
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    })
+    if(res.data.success){
+      //now send the menu item data to the server with the image url
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: data.price,
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      }
+      const menuRes = await axiosSecure.patch(`menu/${_id}`, menuItem)
+      console.log(menuRes.data)
+      reset()
+      if(menuRes.data.modifiedCount > 0){
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${data.name} is updated to the menu`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
     }
+    console.log('with img url', res.data)
   };
 
   return (
     <div>
       <SectionTitle heading="Update Item" subHeading="Refresh Info" />
-      {item ? (
-        <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="p-4">
+        <div className="form-control w-full my-6">
+          <div className="label">
+            <span className="label-text">Recipe Name</span>
+          </div>
           <input
+            {...register("name", {required: true})}
+            required
             type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            defaultValue={name}
+            placeholder="Receipe Name"
+            className="input input-bordered w-full"
           />
-          <input
-            type="number"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-          <button onClick={handleUpdate}>Update Item</button>
         </div>
-      ) : (
-        <p>Loading item data...</p>
-      )}
+        <div className="flex gap-6">
+          <div className="form-control w-full my-6">
+            <div className="label">
+              <span className="label-text">Category</span>
+            </div>
+            <select defaultValue={category}
+              {...register("category", {required: true})}
+              className="select select-bordered w-full"
+            >
+              <option disabled value="default">
+                Select a Category?
+              </option>
+              <option value="salad">Salad</option>
+              <option value="pizza">Pizza</option>
+              <option value="soup">Soup</option>
+              <option value="dessert">Dessert</option>
+              <option value="drinks">Drinks</option>
+            </select>
+          </div>
+          <div className="form-control w-full my-6">
+            <div className="label">
+              <span className="label-text">Receipe Price</span>
+            </div>
+            <input
+              {...register("price", {required: true})}
+              defaultValue={price}
+              type="text"
+              placeholder="Price Name"
+              className="input input-bordered w-full"
+            />
+          </div>
+        </div>
+        <div>
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Recipe Details</span>
+            </div>
+            <textarea
+            defaultValue={recipe}
+              {...register("recipe")}
+              className="textarea textarea-bordered h-24"
+              placeholder="Bio"
+            ></textarea>
+          </label>
+        </div>
+        <div className="my-4">
+          <input
+            {...register("image", {required: true})}
+            type="file"
+            className="file-input w-full max-w-xs"
+          />
+        </div>
+        <button className="btn">
+          Update Menu Item <FaUtensils></FaUtensils>
+        </button>
+      </form>
     </div>
   );
 };
