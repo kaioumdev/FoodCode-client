75d449db888cb041fc39b03ddf1943e95a84410f
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-10 16:39:36 +0600

    feat(payment): get payment information by implementing stripe at payment page

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,15 +1,18 @@
 import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
 
-const stripePromise = loadStripe()
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK)
 
 const Payment = () => {
   return <div>
     <SectionTitle heading="Payment" subHeading="Please pay to eat"></SectionTitle>
     <div>
-    <Elements stripe={stripePromise}></Elements>
+    <Elements stripe={stripePromise}>
+      <CheckoutForm></CheckoutForm>
+    </Elements>
     </div>
   </div>;
 };
