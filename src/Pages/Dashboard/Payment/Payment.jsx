b42c9f2c2dac6c537ac50fc818d56c26f06ba332
kaiyumdev <afrankaiyum34@gmail.com>
2024-07-10 15:04:23 +0600

    feat(checkOut): create checkOut page for payment intregation

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,8 +1,16 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+
+
+const stripePromise = loadStripe()
 
 const Payment = () => {
   return <div>
     <SectionTitle heading="Payment" subHeading="Please pay to eat"></SectionTitle>
+    <div>
+    <Elements stripe={stripePromise}></Elements>
+    </div>
   </div>;
 };
 
