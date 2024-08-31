9bdbb99a7908dd6a31cde9fde20f26ba95c5d365
kaiyumdev <afrankaiyum34@gmail.com>
2024-08-31 18:53:48 +0600

    feat(admin): updated useAdmin method

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -4,6 +4,7 @@ import useAxiosSecure from "./useAxiosSecure";
 
 const useAdmin = () => {
     const {user, loading} = useAuth();
+    // console.log(user);
     const axiosSecure = useAxiosSecure();
     const {data: isAdmin, isPending: isAdminLoading} = useQuery({
         queryKey: [user?.email, 'isAdmin'],
