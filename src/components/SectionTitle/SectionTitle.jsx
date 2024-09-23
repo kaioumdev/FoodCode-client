9cc5c6d516df19f6d88a5b696a0981534e38cf2f
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-23 14:20:58 +0600

    feat(sectionTitle): updated sectionTitle successfully

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
 
-const SectionTitle = ({heading, subHeading}) => {
+const SectionTitle = ({ heading, subHeading }) => {
   return (
-    <div className='md:w-4/12 mx-auto text-center my-8'>
-        <p className='text-yellow-600 mb-2'>-------- {subHeading} --------</p>
-        <h3 className='border-y-4 py-4 text-3xl uppercase'>{heading}</h3>
+    <div className="md:w-4/12 mx-auto text-center my-8">
+      <p className="text-yellow-600 mb-2">-------- {subHeading} --------</p>
+      <h3 className="border-y-4 py-4 text-3xl uppercase">{heading}</h3>
     </div>
-  )
-}
+  );
+};
 
-export default SectionTitle
+export default SectionTitle;
