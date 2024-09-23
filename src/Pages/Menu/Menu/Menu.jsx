2eaa7a51343de9b5afddbe59c40a960e7e1e831b
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-23 19:05:10 +0600

    Feat(cover): remove unusual code & commit from cover file

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,38 +1,43 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import Cover from '../../Shared/Cover/Cover'
-import menuImg from "../../../assets/menu/banner3.jpg"
-import useMenu from '../../../hooks/useMenu'
-import SectionTitle from '../../../components/SectionTitle/SectionTitle'
-import MenuCategory from '../MenuCategory/MenuCategory'
-import dessertImg from "../../../assets/menu/dessert-bg.jpeg"
-import pizzaImg from "../../../assets/menu/pizza-bg.jpg"
-import saladImg from "../../../assets/menu/salad-bg.jpg"
-import soupImg from "../../../assets/menu/soup-bg.jpg"
+import { Helmet } from "react-helmet";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
 
 const Menu = () => {
   const [menu] = useMenu();
-  const desserts = menu.filter((item) => item.category === 'dessert');
-  const salads = menu.filter((item) => item.category === 'salad');
-  const pizzas = menu.filter((item) => item.category === 'pizza');
-  const offered = menu.filter((item) => item.category === 'offered');
-  const soups = menu.filter((item) => item.category === 'soup');
-
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const salads = menu.filter((item) => item.category === "salad");
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+  const soups = menu.filter((item) => item.category === "soup");
 
   return (
     <div>
-        <Helmet>
-            <title>Bistro Boss | Menu</title>
-        </Helmet>
-        <Cover img={menuImg} title="Our Menu"></Cover>
-        <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
-        <MenuCategory items={offered}></MenuCategory>
-        <MenuCategory title="desserts" items={desserts} img={dessertImg}></MenuCategory>
-        <MenuCategory title="pizzas" items={pizzas} img={pizzaImg}></MenuCategory>
-        <MenuCategory title="salads" items={salads} img={saladImg}></MenuCategory>
-        <MenuCategory title="soups" items={soups} img={soupImg}></MenuCategory>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+      <SectionTitle
+        subHeading="Don't Miss"
+        heading="Today's Offer"
+      ></SectionTitle>
+      <MenuCategory items={offered}></MenuCategory>
+      <MenuCategory
+        title="desserts"
+        items={desserts}
+        img={dessertImg}
+      ></MenuCategory>
+      <MenuCategory title="pizzas" items={pizzas} img={pizzaImg}></MenuCategory>
+      <MenuCategory title="salads" items={salads} img={saladImg}></MenuCategory>
+      <MenuCategory title="soups" items={soups} img={soupImg}></MenuCategory>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
