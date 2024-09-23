b637094600bae49f8ca34976ac52a370ff59ea60
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-23 14:15:01 +0600

    feat(main): remove unusual code from main file

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
-import Navbar from '../Pages/Shared/Navbar/Navbar'
-import Footer from '../Pages/Shared/Footer/Footer'
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
-  const location = useLocation()
-  const noHeaderFooter = location.pathname.includes("login") || location.pathname.includes("signup")
+  const location = useLocation();
+  const noHeaderFooter =
+    location.pathname.includes("login") || location.pathname.includes("signup");
   return (
     <div>
-       {noHeaderFooter || <Navbar></Navbar>}
-        <Outlet></Outlet>
-       {noHeaderFooter || <Footer></Footer>}
+      {noHeaderFooter || <Navbar></Navbar>}
+      <Outlet></Outlet>
+      {noHeaderFooter || <Footer></Footer>}
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
