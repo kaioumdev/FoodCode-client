6af07ca9562624db1f552ac020360f61848ce09b
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-19 17:31:31 +0600

    feat(pagination): create pagination functionality & successfully work the pagination function

--- src/Pages/Order/OrderTab/OrderTab.jsx
+++ src/Pages/Order/OrderTab/OrderTab.jsx
@@ -1,32 +1,47 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import FoodCard from "../../../components/FoodCard/FoodCard";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
 
 const OrderTab = ({ items }) => {
-  const pagination = {
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
-    },
+  const [currentPage, setCurrentPage] = useState(0); // State for the current page
+  const itemsPerPage = 6; // Number of items to display per page
+
+  // Calculate the items to display for the current page
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = items.slice(startIndex, endIndex);
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(items.length / itemsPerPage);
+
+  // Handle page button clicks
+  const handlePageClick = (pageIndex) => {
+    setCurrentPage(pageIndex);
   };
+
   return (
     <div>
-      <Swiper
-        pagination={pagination}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className="grid md:grid-cols-3 gap-10">
-            {items.map((item) => (
-              <FoodCard key={item._id} item={item}></FoodCard>
-            ))}
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      <div className="grid md:grid-cols-3 gap-10">
+        {currentItems.map((item, index) => (
+          <FoodCard key={index} item={item}></FoodCard>
+        ))}
+      </div>
+      <div className="flex justify-center mt-8 my-4">
+        {/* Render pagination buttons */}
+        {Array.from(
+          { length: totalPages },
+          (_, index) => (
+            <button
+              key={index}
+              onClick={() => handlePageClick(index)}
+              className={`btn mx-1 ${currentPage === index ? "btn-primary" : "btn-outline"
+                }`}
+            >
+              {index + 1}
+            </button>
+          )
+        )}
+      </div>
     </div>
   );
 };
