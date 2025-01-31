e3fccab898530ebabc89191a14421395ec3cdf07
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-31 19:15:16 +0600

    feat(chartData): show chartData from order-stats fetching by useQuery

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -13,6 +13,15 @@ const AdminHome = () => {
       return res.data;
     },
   });
+
+  const { data: chartData = [] } = useQuery({
+    queryKey: ["order-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/order-stats");
+      return res.data;
+    },
+  });
+
   return (
     <div>
       <h2 className="text-3xl">
