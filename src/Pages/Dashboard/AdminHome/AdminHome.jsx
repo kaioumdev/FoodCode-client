9260c8981b0924573cad803a4185dfdb731dce41
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-31 19:17:37 +0600

    custorm shape for the bar chart

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
@@ -21,6 +22,22 @@ const AdminHome = () => {
       return res.data;
     },
   });
+
+  //custorm shape for the bar chart
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2
+      },${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width
+      }, ${y + height}
+  Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
 
   return (
     <div>
