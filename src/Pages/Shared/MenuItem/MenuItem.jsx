435c3d70e178c2f1acb69d3809f74599cf97b9a5
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-04 00:09:15 +0600

    feat(menuItem): improve menuItem styles to make menu responsive for all device

--- src/Pages/Shared/MenuItem/MenuItem.jsx
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -1,28 +1,3 @@
-// /* eslint-disable react/prop-types */
-
-// const MenuItem = ({ item }) => {
-//   const { name, image, recipe, price } = item;
-//   return (
-//     <div>
-//       <div className="flex items-center">
-//         <img
-//           style={{ borderRadius: "0 200px 200px 200px" }}
-//           className="w-16 h-16"
-//           src={image}
-//           alt={image}
-//         />
-//         <div>
-//           <h4 className="text-xl font-bold">{name}</h4>
-//           <p>{recipe}</p>
-//         </div>
-//         <span className="text-yellow-600">{price}</span>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MenuItem;
-
 /* eslint-disable react/prop-types */
 const MenuItem = ({ item }) => {
   const { name, image, recipe, price } = item;
