ced9099c292c90a20b46afbac7518cf8c4886fcf
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-03 21:05:49 +0600

    feat(sectionTitle): make sectionTitle fully responsive for all device successfully

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
-
 const SectionTitle = ({ heading, subHeading }) => {
   return (
-    <div className="md:w-4/12 mx-auto text-center my-8">
-      <p className="text-yellow-600 mb-2">-------- {subHeading} --------</p>
-      <h3 className="border-y-4 py-4 text-3xl uppercase">{heading}</h3>
+    <div className="w-10/12 md:w-4/12 mx-auto text-center my-6 md:my-8">
+      <p className="text-yellow-600 mb-1 md:mb-2 text-sm md:text-base">-------- {subHeading} --------</p>
+      <h3 className="border-y-2 md:border-y-4 py-3 md:py-4 text-xl md:text-3xl uppercase">
+        {heading}
+      </h3>
     </div>
   );
 };
