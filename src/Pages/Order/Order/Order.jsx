030592f913dde42203aeebf73f830bf3501cf9ba
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-04 14:34:49 +0600

    feat(order): remove padding from order main section to align cover image successfully at navbar

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -24,7 +24,7 @@ const Order = () => {
   const offered = menu.filter((item) => item.category === "offered");
 
   return (
-    <div className="px-4 sm:px-6 md:px-10 lg:px-16 py-6">
+    <div>
       <Helmet>
         <title>FoodCode | Order</title>
       </Helmet>
