7eeaa6a4aac8107b62961930d8c21be9df12e18c
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-05 21:30:42 +0600

    feat(useUser): create useUser hooks to use user dynamically

---
+++ src/hooks/useUser.jsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
+
+const useUsers = () => {
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ["users"],
+        queryFn: async () => {
+            const res = await useAxiosSecure.get("/users");
+            return res?.data;
+        },
+    });
+
+    return [users, refetch];
+};
+
+export default useUsers;
