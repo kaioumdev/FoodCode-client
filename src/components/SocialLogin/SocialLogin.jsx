e737d61d9993e55089c3d08297c2dee2a5a91071
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-07 11:23:10 +0600

    feat(hooks): create new hooks useSocialLogin to use authentication system dynamically

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,29 +1,2 @@
-import useAxiosPublic from "../../hooks/useAxiosPublic";
-import { useNavigate } from "react-router-dom";
 
-const useSocialLogin = () => {
-  const axiosPublic = useAxiosPublic();
-  const navigate = useNavigate();
-  const handleSocialLogin = (provider) => {
-    provider()
-      .then(result => {
-        console.log(result.user);
-        const userInfo = {
-          email: result.user?.email,
-          name: result.user?.displayName,
-          photoURL: result.user?.photoURL
-        }
-        axiosPublic.post('/users', userInfo)
-          .then(res => {
-            console.log(res.data);
-            navigate('/');
-          })
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  return { handleSocialLogin }
-};
 
-export default useSocialLogin;
---
+++ src/hooks/useSocialLogin.jsx
@@ -0,0 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import useAxiosPublic from "./useAxiosPublic";
+
+const useSocialLogin = () => {
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
+    const handleSocialLogin = (provider) => {
+        provider()
+            .then(result => {
+                console.log(result.user);
+                const userInfo = {
+                    email: result.user?.email,
+                    name: result.user?.displayName,
+                    photoURL: result.user?.photoURL
+                }
+                axiosPublic.post('/users', userInfo)
+                    .then(res => {
+                        console.log(res.data);
+                        navigate('/');
+                    })
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    return { handleSocialLogin }
+};
+
+export default useSocialLogin;
