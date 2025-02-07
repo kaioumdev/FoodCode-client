0e65a8df7e982be21320dd76f342104f267db6a0
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-07 12:15:38 +0600

    feat(isAdmin): got isAdmin status from the serber by api request successfully

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -10,9 +10,9 @@ const useAdmin = () => {
     queryKey: [user?.email, "isAdmin"],
     enabled: !loading,
     queryFn: async () => {
-      console.log("asking or checking is admin", user, isAdmin);
+      // console.log("asking or checking is admin", user, isAdmin);
       const res = await axiosSecure.get(`/users/admin/${user.email}`);
-      console.log("check is admin or not", res.data);
+      // console.log("check is admin or not", res.data);
       return res.data?.role;
     },
   });
