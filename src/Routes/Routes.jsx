0289fa3e89a35fbc82e605b19cff0a22598bfe12
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-07 16:24:22 +0600

    implements v7_startTransition to solve React Router Future Flag Warnin

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -134,4 +134,11 @@ export const router = createBrowserRouter([
       },
     ],
   },
-]);
+],
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    },
+  }
+);
