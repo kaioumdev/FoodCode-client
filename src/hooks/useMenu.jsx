21d474d600e49a6620b649830fe40483fd489db4
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-08 19:23:02 +0600

    feat(menu): got menu using react-query to replace useEffect

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -3,24 +3,6 @@ import useAxiosPublic from "./useAxiosPublic";
 
 const useMenu = () => {
   const axiosPublic = useAxiosPublic();
-  // const [menu, setMenu] = useState([])
-  // const [loading, setLoading] = useState(true)
-  // const [error, setError] = useState(null)
-
-  // useEffect(() => {
-  //     fetch('http://localhost:5000/menu')
-  //    .then(res => res.json())
-  //    .then(data => {
-  //        setMenu(data)
-  //        setLoading(false)
-  //    })
-  //    .catch(error => {
-  //        setError(error)
-  //        setLoading(false)
-  //    })
-  // }, [])
-
-  // return [menu, loading, error];
   const {
     data: menu = [],
     isPending: loding,
