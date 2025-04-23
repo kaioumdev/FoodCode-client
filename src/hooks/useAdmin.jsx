e2d6bf13c76ab33caf67cd462c02b7457537e35d
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-23 19:23:23 +0600

    useAdmin hooks clean & remove console from useAdmin hook

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -4,15 +4,12 @@ import useAxiosSecure from "./useAxiosSecure";
 
 const useAdmin = () => {
   const { user, loading } = useAuth();
-  //   console.log(user);
   const axiosSecure = useAxiosSecure();
   const { data: isAdmin, isPending: isAdminLoading } = useQuery({
     queryKey: [user?.email, "isAdmin"],
     enabled: !loading,
     queryFn: async () => {
-      // console.log("asking or checking is admin", user, isAdmin);
       const res = await axiosSecure.get(`/users/admin/${user.email}`);
-      // console.log("check is admin or not", res.data);
       return res.data?.role;
     },
   });
